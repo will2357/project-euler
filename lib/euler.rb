8edcd906be4c2049dd7482c36e04c16e847558b2
will2357 <will@highducheck.org>
2024-01-18 16:54:43 -0500

    DIY rspec

---
+++ lib/euler.rb
@@ -0,0 +1,7 @@
+class Euler
+  attr_reader :number
+
+  def initialize(number: nil)
+    @number = number
+  end
+end
---
+++ spec/euler.rb
@@ -0,0 +1,12 @@
+require 'euler'
+#require 'rspec/autorun'
+
+describe Euler do
+  question = Euler.new(number: 1)
+
+  context "creates the first question and answer" do
+    it "has the getter for number" do
+      expect(question.number).to eq(1)
+    end
+  end
+end
